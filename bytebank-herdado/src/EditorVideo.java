7d6fc01725d85712056e5afb0a95807348c47faa
Haynan Penha <haynan.mesquita@gmail.com>
2023-02-15 16:22:26 -0300

    Criação do projeto bytebank-herdado

---
+++ bytebank-herdado/src/EditorVideo.java
@@ -0,0 +1,7 @@
+public class EditorVideo extends Funcionario {
+        
+    public double getBonificacao() {
+        System.out.println("Chamando o metodo de bonificacao do editor de video");
+        return super.getBonificacao() + 100;
+    }
+}
