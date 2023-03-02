09381ddf7c9ea3a150394bdaa9af221db63dd3b7
Haynan Penha <haynan.mesquita@gmail.com>
2023-03-02 15:27:05 -0300

    Conclusão de Collections

--- gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
+++ gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
@@ -1,5 +1,8 @@
 package br.com.alura;
 
+// import java.util.Iterator;
+// import java.util.Set;
+
 public class TestaCursoComAluno {
     public static void main(String[] args) {
         Curso javaColecoes = new Curso("Dominando as coleções do Java", "Haynan Penha");
@@ -17,8 +20,34 @@ public class TestaCursoComAluno {
         javaColecoes.matricula(a3);
 
         System.out.println("Todos os alunos matriculados: ");
+
         javaColecoes.getAlunos().forEach(a ->{
             System.out.println(a);
         });
+
+        /* Set<Aluno> alunos = javaColecoes.getAlunos();
+        Iterator<Aluno> iterador = alunos.iterator();
+        while (iterador.hasNext()) {
+            Aluno proximo = iterador.next();
+            System.out.println(proximo);
+        } */
+
+        /* for(Aluno a : javaColecoes.getAlunos()) {
+            System.out.println(a);
+        } */
+
+        System.out.println("O aluno " + a1 + " está matriculado?");
+        System.out.println(javaColecoes.estaMatriculado(a1));
+
+        Aluno turini = new Aluno("Rodrigo Turini", 34672);
+        System.out.println("E esse Turini, está matriculado?");
+        System.out.println(javaColecoes.estaMatriculado(turini));
+
+        System.out.println("O a1 é equals ao turini?");
+        System.out.println(a1.equals(turini));
+
+        // obrigatoriamente o seguinte é true:
+
+        System.out.println(a1.hashCode() == turini.hashCode());
     }
 }
